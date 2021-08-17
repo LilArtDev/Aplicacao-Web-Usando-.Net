777212142b3ae0434fdbf17501bfcd74575e9d47
Arthur Fernandez <arthur.fa.silva1@gmail.com>
2021-08-17 17:04:39 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+    }
+}
